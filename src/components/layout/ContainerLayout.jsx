27cf2b461f84990bc2b7826bcb814ddcdd756030
Sultan Fariz <sultan_fariz@apps.ipb.ac.id>
2022-02-17 12:14:53 +0700

    fix: adjust page setup

--- src/components/layout/ContainerLayout.jsx
+++ src/components/layout/ContainerLayout.jsx
@@ -29,23 +29,3 @@ export const ContainerLayout = ({ children }) => {
 ContainerLayout.propTypes = {
   children: PropTypes.oneOfType([PropTypes.array, PropTypes.element]),
 };
-
-// import React from 'react';
-// import { Container, Box } from '@mui/material';
-// import PropTypes from 'prop-types';
-// import { useStyles } from './ContainerLayoutStyles';
-
-// export const ContainerLayout = ({ children }) => {
-//   const classes = useStyles();
-//   return (
-//     <Box>
-//       <Container className={classes.root}>
-//         <Box>{children}</Box>
-//       </Container>
-//     </Box>
-//   );
-// };
-
-// ContainerLayout.propTypes = {
-//   children: PropTypes.oneOfType([PropTypes.array, PropTypes.element]),
-// };
